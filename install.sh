32466f9e3b3df695d5c2f21b9d9195555cdccf14
Retro <59291834+RetroPlayerYT@users.noreply.github.com>
2022-09-15 16:39:19 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,9 +13,11 @@ sudo wget --no-check-certificate "https://libchl.netlify.app/ch3"
 sudo wget --no-check-certificate "https://libchl.netlify.app/chl"
 sudo wget --no-check-certificate "https://libchl.netlify.app/chs"
 sudo wget --no-check-certificate "https://libchl.netlify.app/chd"
+sudo wget --no-check-certificate "https://libchl.netlify.app/chli"
 sudo chmod +x ch1
 sudo chmod +x ch2
 sudo chmod +x ch3
 sudo chmod +x chl
 sudo chmod +x chs
 sudo chmod +x chd
+sudo chmod +x chli
